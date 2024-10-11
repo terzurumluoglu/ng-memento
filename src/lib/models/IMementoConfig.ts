5cfd7608a85ff6c5af4422df59793c132a29b985
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2024-10-12 00:31:52 +0300

    used types

--- src/lib/models/IMementoConfig.ts
+++ src/lib/models/IMementoConfig.ts
@@ -1,6 +1,9 @@
+import { storeType } from "../types/store.type";
 import { IMethodPath } from "./IMethodPath";
 
 export interface IMementoConfig {
   expireTimeAsMilliSeconds: number;
   paths: IMethodPath[];
+  store?: storeType;
+  storeKey?: string;
 }
