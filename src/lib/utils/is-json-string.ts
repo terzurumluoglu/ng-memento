f0171f72b61829667bd7fc456197d9bdf7d12990
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2024-10-21 15:30:13 +0300

    fix undefined as generic

--- src/lib/utils/is-json-string.ts
+++ src/lib/utils/is-json-string.ts
@@ -2,6 +2,6 @@ export const isJsonString = <T>(str: string) => {
   try {
     return JSON.parse(str) as T;
   } catch (error) {
-    return undefined as T;
+    return undefined as unknown as T;
   }
 };
